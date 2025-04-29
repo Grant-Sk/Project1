9fae9145e1b243d76b4330c2734486fd623a696d
Grant-Sk <grantskoumal@gmail.com>
2025-04-28 22:21:19 -0500

    gui and main
    made the GUI and main. and made the files for tests and logic

---
+++ logic.py
@@ -0,0 +1,8 @@
+class Logic:
+    def __init__(self):
+        id_list.self = []
+    def ID_verify(self, user_id):
+        user_id.self = user_id
+    
+
+
---
+++ main.py
@@ -0,0 +1,12 @@
+from voteing_gui import *
+import sys
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.setFixedSize(650, 600)
+    MainWindow.show()
+    sys.exit(app.exec())
+if __name__ == '__main__':
+    main()
