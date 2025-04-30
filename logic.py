beef7a4335482a76e1189f82f1d37cd4f3bd90d3
Grant-Sk <grantskoumal@gmail.com>
2025-04-29 20:33:45 -0500

    Update logic.py
    added to logic function

--- logic.py
+++ logic.py
@@ -1,8 +1,25 @@
+import csv
+from voteing_gui import *
+
 class Logic:
-    def __init__(self):
-        id_list.self = []
-    def ID_verify(self, user_id):
-        user_id.self = user_id
-    
+    def __init__(self, vote, voter_id):
+        self.vote = vote
+        self.voter_id = voter_id
+    def id_verify(self):
+        with open("vote.txt", "r") as file:
+            for i in file.readlines():
+                if i.strip() == self.voter_id:
+                    Ui_MainWindow().label_4.setText(QtCore.QCoreApplication.translate(
+                        "MainWindow",'<span style="color:red;">Already Voted</span>'))
+                    return False
+        self.add_vote()
+        return True
+    def add_vote(self):
+        with open("vote.txt", "a") as file:
+            writer = csv.writer(file)
+            writer.writerow([self.voter_id, self.vote])
+    def return_vote(self):
+        with open("vote.txt", "r") as file:
+            pass
 
 
